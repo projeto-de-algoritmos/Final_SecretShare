b5320a9d59c3627e7fe36936aaaaab4bb6ec0d18
Felipe Campos <fepas.unb@gmail.com>
2021-10-26 00:00:45 -0300

    removing non used things
    Signed-off-by: Felipe Campos <fepas.unb@gmail.com>

--- client/src/components/Link/styles.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const ContentLink = styled.div`
-  margin: 1rem 0rem 1rem 0rem;
-`;
--- client/src/components/Logo/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SecretLogo from '../../assets/logo.png';
-
-// eslint-disable-next-line react/prop-types
-export default function Logo() {
-  return (
-    <img src={SecretLogo} alt="Logo" />
-  );
-}
